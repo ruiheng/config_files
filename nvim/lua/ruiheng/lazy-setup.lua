13f6c261e317eac28303aa228dd82b03c4431030
Gilbert Fine <gilbert.fine@gmail.com>
2023-08-04 19:31:20 +0800

    new plugin: indent-blankline.nvim

--- nvim/lua/ruiheng/lazy-setup.lua
+++ nvim/lua/ruiheng/lazy-setup.lua
@@ -124,6 +124,27 @@ require("lazy").setup({
 
     -- general programming ---
 
+    { "lukas-reineke/indent-blankline.nvim",
+      config = function()
+        vim.opt.termguicolors = true
+        vim.cmd [[highlight IndentBlanklineIndent1 guibg=#1f1f1f gui=nocombine]]
+        vim.cmd [[highlight IndentBlanklineIndent2 guibg=#1a1a1a gui=nocombine]]
+
+        require("indent_blankline").setup {
+            char = "",
+            char_highlight_list = {
+                "IndentBlanklineIndent1",
+                "IndentBlanklineIndent2",
+            },
+            space_char_highlight_list = {
+                "IndentBlanklineIndent1",
+                "IndentBlanklineIndent2",
+            },
+            show_trailing_blankline_indent = false,
+        }
+      end,
+    },
+
     { "neomake/neomake",
       config = function()
       end
