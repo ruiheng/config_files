6235c07add2ec940cce27005e257afff63a6458a
Gilbert Fine <gilbert.fine@gmail.com>
2023-10-07 22:23:41 +0800

    add new plugin: LuaSnip and haskell-snippets

--- nvim/lua/ruiheng/lazy-setup.lua
+++ nvim/lua/ruiheng/lazy-setup.lua
@@ -172,6 +172,38 @@ require("lazy").setup({
       end,
     },
 
+    'mrcjkb/haskell-snippets.nvim',
+
+    {
+      "L3MON4D3/LuaSnip",
+      -- follow latest release.
+      version = "2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+      -- install jsregexp (optional!).
+      build = "make install_jsregexp",
+      config = function ()
+        local ls = require("luasnip")
+        ls.setup {}
+        vim.keymap.set({"i"}, "<C-K>", function() ls.expand() end, {silent = true})
+        vim.keymap.set({"i", "s"}, "<C-L>", function() ls.jump( 1) end, {silent = true})
+        vim.keymap.set({"i", "s"}, "<C-J>", function() ls.jump(-1) end, {silent = true})
+
+        vim.keymap.set({"i", "s"}, "<C-E>", function()
+          if ls.choice_active() then
+            ls.change_choice(1)
+          end
+        end, {silent = true})
+
+        require('luasnip.loaders.from_snipmate').lazy_load()
+
+        local ok, haskell_snippets = pcall(require, 'haskell-snippets')
+        if ok then
+          haskell_snippets = haskell_snippets.all
+          ls.add_snippets('haskell', haskell_snippets, { key = 'haskell' })
+        end
+
+      end
+    },
+
     { "neomake/neomake",
       config = function()
       end
