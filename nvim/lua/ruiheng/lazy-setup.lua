a6cc76996eda33a5b082112941506ae59cb358f7
Gilbert Fine <gilbert.fine@gmail.com>
2023-10-11 21:27:11 +0800

    migrate config for plugin 'indent-blankline'

--- nvim/lua/ruiheng/lazy-setup.lua
+++ nvim/lua/ruiheng/lazy-setup.lua
@@ -152,22 +152,17 @@ require("lazy").setup({
     },
 
     { "lukas-reineke/indent-blankline.nvim",
+      main = 'ibl',
       config = function()
         vim.opt.termguicolors = true
         vim.cmd [[highlight IndentBlanklineIndent1 guibg=#1f1f1f gui=nocombine]]
         vim.cmd [[highlight IndentBlanklineIndent2 guibg=#1a1a1a gui=nocombine]]
 
-        require("indent_blankline").setup {
-            char = "",
-            char_highlight_list = {
-                "IndentBlanklineIndent1",
-                "IndentBlanklineIndent2",
-            },
-            space_char_highlight_list = {
-                "IndentBlanklineIndent1",
-                "IndentBlanklineIndent2",
-            },
-            show_trailing_blankline_indent = false,
+        require("ibl").setup {
+           debounce = 100,
+           indent = { char = "|" },
+           whitespace = { highlight = { "Whitespace", "NonText" } },
+           scope = { exclude = { language = { "lua" } } },
         }
       end,
     },
