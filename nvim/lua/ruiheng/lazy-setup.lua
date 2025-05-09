543b5826b0c6a6e50b8023092320b5296b16483d
Gilbert Fine <gilbert.fine@gmail.com>
2025-05-09 10:59:15 +0800

    disable 'nvim-treesitter/nvim-treesitter-context'
    because it take too much space sometimes (e.g. when function's signature has many lines)

--- nvim/lua/ruiheng/lazy-setup.lua
+++ nvim/lua/ruiheng/lazy-setup.lua
@@ -298,6 +298,7 @@ require("lazy").setup({
     },
 
     { 'nvim-treesitter/nvim-treesitter-context',
+      enabled = false,
       config = function()
         vim.keymap.set("n", "[c", function()
           require("treesitter-context").go_to_context()
