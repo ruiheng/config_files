c512a5561609c54c909e1f8b08dea91a7b6bae92
Gilbert Fine <gilbert.fine@gmail.com>
2025-05-10 16:01:43 +0800

    new style of lspconfig configs

--- nvim/lua/ruiheng/plugin_setup/lspconfig.lua
+++ nvim/lua/ruiheng/plugin_setup/lspconfig.lua
@@ -1,26 +1,9 @@
 local M = {}
 
 M.config = function ()
-  local lspconfig = require('lspconfig')
-
-  local on_attach = function(client, bufnr)
-    if client.name == 'ruff_lsp' then
-      -- Disable hover in favor of Pyright
-      client.server_capabilities.hoverProvider = false
-    end
-  end
-
-  lspconfig.ruff.setup {
-    -- on_attach = on_attach,
-  }
-
-  -- lspconfig.pyright.setup { }
-  lspconfig.basedpyright.setup { }
-
-  lspconfig.ts_ls.setup {}
-
-  -- use either this or haskell-tools
-  -- lspconfig.hls.setup {}
+  vim.lsp.enable('basedpyright')
+  vim.lsp.enable('ruff')
+  vim.lsp.enable('ts_ls')
 end
 
 
