2fd210ef95b3feda77f35852effedd1e9aca63d8
Gilbert Fine <gilbert.fine@gmail.com>
2025-06-22 01:24:29 +0800

    replace 'leap.nvim' with 'flash.nvim'

--- nvim/lua/ruiheng/lazy-setup.lua
+++ nvim/lua/ruiheng/lazy-setup.lua
@@ -85,6 +85,7 @@ require("lazy").setup({
     },
 
     { 'ggandor/leap.nvim',
+      enabled = false, -- use 'flash.nvim' instead
       config = function ()
         local leap = require('leap')
         leap.set_default_mappings()
@@ -96,6 +97,27 @@ require("lazy").setup({
             )
           end
       end
+    },
+
+    {
+      "folke/flash.nvim",
+      event = "VeryLazy",
+      ---@type Flash.Config
+      opts = {
+        modes = {
+          search = {
+            enabled = true,
+          },
+        },
+      },
+      -- stylua: ignore
+      keys = {
+        { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+        { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+        { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+        { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+        { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+      },
     },
 
     -- terminal --
