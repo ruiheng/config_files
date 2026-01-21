41fdc187f4afe5306cb3b90ae838dc4f41288961
Gilbert Fine <gilbert.fine@gmail.com>
2026-01-21 15:09:23 +0800

    blink.cmp: enable snippets: luasnip

--- nvim/lua/ruiheng/lazy-setup.lua
+++ nvim/lua/ruiheng/lazy-setup.lua
@@ -161,6 +161,8 @@ require("lazy").setup({
         -- (Default) Only show the documentation popup when manually triggered
         completion = { documentation = { auto_show = false } },
 
+        snippets = { preset = 'luasnip' },
+
         -- Default list of enabled providers defined so that you can extend it
         -- elsewhere in your config, without redefining it, due to `opts_extend`
         sources = {
