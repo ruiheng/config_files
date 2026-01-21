6ab4e548086548808144e222895a28d261e078c3
Gilbert Fine <gilbert.fine@gmail.com>
2026-01-21 19:39:57 +0800

    nvim: tabnine: use <C-Tab> to accept

--- nvim/lua/ruiheng/lazy-setup.lua
+++ nvim/lua/ruiheng/lazy-setup.lua
@@ -913,7 +913,7 @@ require("lazy").setup({
       config = function ()
         require('tabnine').setup({
           disable_auto_comment=true,
-          accept_keymap="<Tab>",
+          accept_keymap="<C-Tab>",
           dismiss_keymap = "<C-]>",
           debounce_ms = 800,
           suggestion_color = {gui = "#808080", cterm = 244},
