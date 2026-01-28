672a9736894ee800b5b7807f75fe58ce46bfce16
Gilbert Fine <gilbert.fine@gmail.com>
2026-01-28 20:05:31 +0800

    update opts of 'trouble.nvim'

--- nvim/lua/ruiheng/lazy-setup.lua
+++ nvim/lua/ruiheng/lazy-setup.lua
@@ -962,20 +962,44 @@ require("lazy").setup({
       end
     },
 
-    { 'folke/trouble.nvim',
-      dependencies = 'nvim-tree/nvim-web-devicons',
+    {
+      "folke/trouble.nvim",
       lazy = true,
       event = { "User LazyLoadProgramming" },
-      config = function(_, opts)
-        local trouble = require('trouble')
-        trouble.setup(opts)
-        vim.keymap.set("n", "<leader>xx", function() trouble.toggle() end)
-        vim.keymap.set("n", "<leader>xw", function() trouble.toggle("workspace_diagnostics") end)
-        vim.keymap.set("n", "<leader>xd", function() trouble.toggle("document_diagnostics") end)
-        vim.keymap.set("n", "<leader>xq", function() trouble.toggle("quickfix") end)
-        vim.keymap.set("n", "<leader>xl", function() trouble.toggle("loclist") end)
-        -- vim.keymap.set("n", "gR", function() trouble.open("lsp_references") end)
-      end
+      opts = {}, -- for default options, refer to the configuration section for custom setup.
+      cmd = "Trouble",
+      keys = {
+        {
+          "<leader>xx",
+          "<cmd>Trouble diagnostics toggle<cr>",
+          desc = "Diagnostics (Trouble)",
+        },
+        {
+          "<leader>xX",
+          "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+          desc = "Buffer Diagnostics (Trouble)",
+        },
+        {
+          "<leader>cs",
+          "<cmd>Trouble symbols toggle focus=false<cr>",
+          desc = "Symbols (Trouble)",
+        },
+        {
+          "<leader>cl",
+          "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+          desc = "LSP Definitions / references / ... (Trouble)",
+        },
+        {
+          "<leader>xL",
+          "<cmd>Trouble loclist toggle<cr>",
+          desc = "Location List (Trouble)",
+        },
+        {
+          "<leader>xQ",
+          "<cmd>Trouble qflist toggle<cr>",
+          desc = "Quickfix List (Trouble)",
+        },
+      },
     },
 
     { 'nvimtools/none-ls.nvim',
