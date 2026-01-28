a96b492712f5c7699758676aee5f1eb10de811b7
Gilbert Fine <gilbert.fine@gmail.com>
2026-01-28 20:04:37 +0800

    nvim-lspconfig: don't setup any LSP because 'mason-lspconfig.nvim' is used now

--- nvim/lua/ruiheng/plugin_setup/lspconfig.lua
+++ nvim/lua/ruiheng/plugin_setup/lspconfig.lua
@@ -1,10 +1,12 @@
 local M = {}
 
 M.config = function ()
-  vim.lsp.enable('basedpyright')
-  vim.lsp.enable('ruff')
+  -- vim.lsp.enable('basedpyright')
+  -- vim.lsp.enable('ty')
+  -- vim.lsp.enable('jedi-language-server')
+  -- vim.lsp.enable('ruff')
   -- vim.lsp.enable('ruff_lsp')
-  vim.lsp.enable('ts_ls')
+  -- vim.lsp.enable('ts_ls')
 end
 
 
